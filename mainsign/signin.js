258be7d6a2f51b426ac5b81c227f6cd2c2012601
paruljamwal <paruljamwal2000@gmail.com>
2022-01-21 15:28:44 +0530

    Update signin.js

--- mainsign/signin.js
+++ mainsign/signin.js
@@ -1,6 +1,5 @@
 
     document.querySelector("#zappobtn").addEventListener("click",gotoacc);
     function gotoacc(){
-        // window.location.href="signup.html";
         window.location.pathname="../signup/signup.html";
     }
